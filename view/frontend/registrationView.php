70992dc570c13e9f497ab7e7bbe9de884806e1ac
AkeraKyuar <elise-layes@laposte.net>
2018-01-09 11:42:37 +0100

    body deleted

--- view/frontend/registrationView.php
+++ view/frontend/registrationView.php
@@ -4,18 +4,17 @@
 <h1>Mon super blog !</h1>
 <p><a href="index.php">Retour à la liste des billets</a></p>
 
-<body>
-    
-    <form action="index.php?action=register" method="post">
-        <p>
-        <label for="pseudo">Pseudo :</label><input type="text" name="pseudo" id="pseudo"><br />
-        <label for="pass">Mot de passe :</label><input type="text" name="pass" id="pass" /><br />
-        <label for="confirmPass">Retapez votre mot de passe :</label><input type="text" name="confirmPass" id="confirmPass" /><br />
-        <label for="email">Adresse e-mail :</label><input type="text" name="email" id="email" /><br />
 
-        <input type="submit" value="Envoyer" />
-	</p>
-    </form>
+<form action="index.php?action=register" method="post">
+    <p>
+    <label for="pseudo">Pseudo :</label><input type="text" name="pseudo" id="pseudo"><br />
+    <label for="pass">Mot de passe :</label><input type="text" name="pass" id="pass" /><br />
+    <label for="confirmPass">Retapez votre mot de passe :</label><input type="text" name="confirmPass" id="confirmPass" /><br />
+    <label for="email">Adresse e-mail :</label><input type="text" name="email" id="email" /><br />
+
+    <input type="submit" value="Envoyer" />
+</p>
+</form>
 
 
 <?php $content = ob_get_clean(); ?>
