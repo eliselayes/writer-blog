51c850495af49f25b768f4347c3286b0b2412568
AkeraKyuar <elise-layes@laposte.net>
2018-01-13 19:33:39 +0100

    parameters added and variables names changed in the array

--- model/RegistrationManager.php
+++ model/RegistrationManager.php
@@ -7,12 +7,12 @@ require_once("model/Manager.php");
 class RegistrationManager extends Manager
 {
 
-    public function postRegistration()
+    public function postRegistration($pseudo, $pass, $email)
     {
         //$pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
         $db = $this->dbConnect();
         $req = $db->prepare('INSERT INTO membres(pseudo, pass, email, date_inscription) VALUES(?, ?, ?, NOW())');
-        $req->execute(array($_POST['pseudo'], $pass_hache, $_POST['email']));
+        $req->execute(array($pseudo, $pass, $email));
 
         
     }
