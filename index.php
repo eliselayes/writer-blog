8dc4ad4818b03b94354d4a3e0d068f888cc03e1b
AkeraKyuar <elise-layes@laposte.net>
2018-01-14 15:52:35 +0100

    direct to the Router object

--- index.php
+++ index.php
@@ -1,55 +1,5 @@
-<?php
-require('controler/frontend.php');
+<?php 
+require_once('model/Router.php');
 
-try {
-    if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'listPosts') {
-            listPosts();
-        }
-        elseif ($_GET['action'] == 'post') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                post();
-            }
-            else {
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
-        elseif ($_GET['action'] == 'addComment') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-                }
-                else {
-                    throw new Exception('Tous les champs ne sont pas remplis !');
-                }
-            }
-            else {
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
-        elseif ($_GET['action'] == 'register') {
-            //if (isset($_POST['pseudo']) && isset($_POST['pass']) && isset($_POST['email'])) {
-                register($_POST['pseudo'], $_POST['pass'], $_POST['email']);
-            /*}
-            else {
-                throw new Exception('Pour vous inscrire, veuillez remplir tous les champs');
-            }*/
-        }
-        elseif ($_GET['action'] == 'login') {
-            if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
-            {
-                echo 'Bonjour ' . $_SESSION['pseudo'];
-            }
-            else {
-                throw new Exception('test');
-            }
-        }
-
-    }
-    else {
-        listPosts();
-    }
-}
-catch(Exception $e) {
-    echo 'Erreur : ' . $e->getMessage();
-}
+$router = new \OpenClassrooms\Blog\Model\Router();
+$router->direct();
