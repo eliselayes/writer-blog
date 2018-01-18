d29dfe9da7138b8762abcccb6d660d651ebfcfb8
AkeraKyuar <elise-layes@laposte.net>
2018-01-18 13:00:53 +0100

    bootstrap elements added

--- view/frontend/logView.php
+++ view/frontend/logView.php
@@ -1,19 +1,31 @@
 <?php $title = 'Mon blog'; ?>
 
 <?php ob_start(); ?>
-<h1>Mon super blog !</h1>
-<p><a href="index.php">Retour à la liste des billets</a></p>
+<div class="container">
+    <div class="row">
 
+        <div class="col-sm-12 blog-header">
+          
 
-<form action="index.php?action=login" method="post">
-    <p>
-    <label for="pseudo">Pseudo :</label><input type="text" name="pseudo" id="pseudo" /><br />
-    <label for="pass">Mot de passe :</label><input type="text" name="pass" id="pass" /><br />
-    
+            <h1 class="blog-title">Le blog de Billet simple pour l'Alaska</h1>
+            <p class="lead blog-description">Roman-feuilleton</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-5 col-sm-offset-4">
+            <h2>Connexion</h2>
+            <p><a href="index.php">Retour à l'accueil du blog</a></p>
 
-    <input type="submit" value="Se connecter" />
-</p>
-</form>
+            <form class="log" action="index.php?action=sendText" method="post">
+                <label for="pseudo">Pseudo :</label><input type="text" name="pseudo" id="pseudo" /><br />
+                <label for="pass">Mot de passe :</label><input type="text" name="pass" id="pass" /><br />
+                
+                <input id="login" type="submit" value="Se connecter" />
+            </form>
+            
+        </div>
+    </div>
+</div>
 
 
 <?php $content = ob_get_clean(); ?>
