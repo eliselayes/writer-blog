7a347ab8f41091006a70896507f6c5025ec48ac0
AkeraKyuar <elise-layes@laposte.net>
2018-01-18 11:31:57 +0100

    old useless directions deleted. login and sendText directions added. directions to functions for empty formulars added

--- model/Router.php
+++ model/Router.php
@@ -9,52 +9,32 @@ class Router
 {
     public function direct()
     {
-        
-
         try {
-            if (isset($_GET['action'])) {
-                if ($_GET['action'] == 'listPosts') {
-                    listPosts();
+            if (isset($_GET['action'])) { 
+                if ($_GET['action'] == 'login') {
+                    if(!empty($_POST['pseudo']) && !empty($_POST['pass'])) {
+                        login($_POST['pseudo'], $_POST['pass']);
+                    }
+                    else {
+                        displayLogin();
+                    }
                 }
-                elseif ($_GET['action'] == 'post') {
+                /*elseif ($_GET['action'] == 'post') {
                     if (isset($_GET['id']) && $_GET['id'] > 0) {
                         post();
                     }
                     else {
                         throw new Exception('Aucun identifiant de billet envoyé');
                     }
-                }
-                elseif ($_GET['action'] == 'addComment') {
-                    if (isset($_GET['id']) && $_GET['id'] > 0) {
-                        if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                            addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-                        }
-                        else {
-                            throw new Exception('Tous les champs ne sont pas remplis !');
-                        }
+                }*/
+                elseif ($_GET['action'] == 'sendText') {
+                    if(!empty($_POST['content'])) {
+                        sendText($_POST['content']);
                     }
                     else {
-                        throw new Exception('Aucun identifiant de billet envoyé');
+                        displaySendText();
                     }
                 }
-                elseif ($_GET['action'] == 'register') {
-                    //if (isset($_POST['pseudo']) && isset($_POST['pass']) && isset($_POST['email'])) {
-                        register($_POST['pseudo'], $_POST['pass'], $_POST['email']);
-                    /*}
-                    else {
-                        throw new Exception('Pour vous inscrire, veuillez remplir tous les champs');
-                    }*/
-                }
-                elseif ($_GET['action'] == 'login') {
-                    if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
-                    {
-                        echo 'Bonjour ' . $_SESSION['pseudo'];
-                    }
-                    else {
-                        throw new Exception('test');
-                    }
-                }
-        
             }
             else {
                 listPosts();
