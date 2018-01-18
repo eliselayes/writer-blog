59099f75c1aa856beffbe89672125d7a5c1c3e77
AkeraKyuar <elise-layes@laposte.net>
2018-01-18 11:36:32 +0100

    dbname changed : writer_blog

--- model/Manager.php
+++ model/Manager.php
@@ -6,7 +6,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+        $db = new \PDO('mysql:host=localhost;dbname=writer_blog;charset=utf8', 'root', '');
         return $db;
     }
 }
