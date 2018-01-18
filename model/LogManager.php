a725397edcbf837f709c9801e0b45f76b23ff420
AkeraKyuar <elise-layes@laposte.net>
2018-01-18 11:39:38 +0100

    deleted for instance, session added

--- model/LogManager.php
+++ model/LogManager.php
@@ -6,18 +6,33 @@ require_once("model/Manager.php");
 
 class LogManager extends Manager
 {
-    
-
-    public function login($pseudo, $pass_hache)
+    public function login($pseudo, $pass)
     {
+        // Verification des identifiants
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id FROM membres WHERE pseudo = ? AND pass = ?');
-        $req->execute(array($pseudo, $pass_hache));
+        $req = $db->prepare('SELECT id FROM authors WHERE pseudo = :pseudo AND pass = :pass');
+        $req->execute(array(
+            'pseudo' => $pseudo,
+            'pass' => $pass));
         
         $resultat = $req->fetch();
+        
+        if (!$resultat)
+        {
+            echo 'Mauvais identifiant ou mot de passe !';
+        }
+        else
+        {
+            session_start();
+            $_SESSION['id'] = $resultat['id'];
+            $_SESSION['pseudo'] = $pseudo;
+            echo 'Vous êtes connecté !';
+        }
+
+
+        if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+        {
+            echo 'Bonjour ' . $_SESSION['pseudo'];
+        }
     }
 }
-
-
-
-
