8789c77cb1004631dd6955ed6052f6dabe4f611b
AkeraKyuar <elise-layes@laposte.net>
2018-01-18 12:58:48 +0100

    function sendText added

--- model/PostManager.php
+++ model/PostManager.php
@@ -9,18 +9,25 @@ class PostManager extends Manager
     public function getPosts()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        $req = $db->query('SELECT id, content, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS creation_date_fr FROM posts ORDER BY date_creation DESC LIMIT 0, 5');
 
         return $req;
     }
-
-    public function getPost($postId)
+    /*public function getPost($postId)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req = $db->prepare('SELECT id, content, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS creation_date_fr FROM posts WHERE id = ?');
         $req->execute(array($postId));
         $post = $req->fetch();
 
         return $post;
+    }*/
+
+    public function sendText($content)
+    {
+        $db = $this->dbConnect();
+        $posts = $db->prepare('INSERT INTO posts(content, date_creation) VALUES(?, NOW())');
+        $posts->execute(array($content));
+
     }
 }
