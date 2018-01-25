d867b7dacd087cd1acd6d5688b900fb69c096285
AkeraKyuar <elise-layes@laposte.net>
2018-01-25 18:20:08 +0100

    affectedLines variable deleted

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -19,8 +19,6 @@ class CommentManager extends Manager
     {
         $db = $this->dbConnect();
         $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
-        $affectedLines = $comments->execute(array($postId, $author, $comment));
-
-        return $affectedLines;
+        $comments->execute(array($postId, $author, $comment));
     }
 }
