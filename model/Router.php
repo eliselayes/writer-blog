b035c63dba6e7525c32a4f8234b34e5d8d67cf9c
AkeraKyuar <elise-layes@laposte.net>
2018-01-25 18:37:29 +0100

    variable controler_frontend added because of the new Frontend class

--- model/Router.php
+++ model/Router.php
@@ -3,39 +3,56 @@
 namespace OpenClassrooms\Blog\Model;
 
 require_once('./index.php');
-require('controler/frontend.php');
+require_once('Frontend.php');
 
 class Router 
 {
     public function direct()
     {
         try {
+            $controler_frontend = new Frontend();
             if (isset($_GET['action'])) { 
                 if ($_GET['action'] == 'login') {
                     if(!empty($_POST['pseudo']) && !empty($_POST['pass'])) {
-                        login($_POST['pseudo'], $_POST['pass']);
+                        $controler_frontend->login($_POST['pseudo'], $_POST['pass']);
+                        //login($_POST['pseudo'], $_POST['pass']);
                     }
                     else {
-                        displayLogin();
+                        $controler_frontend->displayLogin();
                     }
                 }
                 elseif ($_GET['action'] == 'seeOnePost') {
-                    //if (isset($_GET['id']) && $_GET['id'] > 0) {
-                        seeOnePost($_GET['id']);
-                    //}
+                    if (isset($_GET['id']) && $_GET['id'] > 0) {
+                        $controler_frontend->seeOnePost($_GET['id']);
+                    }
                     
                 }
                 elseif ($_GET['action'] == 'sendText') {
                     if(!empty($_POST['content'])) {
-                        sendText($_POST['content']);
+                        $controler_frontend->sendText($_POST['content']);
                     }
                     else {
-                        displaySendText();
+                        $controler_frontend->displaySendText();
                     }
+                }
+                elseif ($_GET['action'] == 'addComment') {
+                    //if (isset($_GET['id']) && $_GET['id'] > 0) {
+                        if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                            //addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                            $controler_frontend->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                        }
+                        else {
+                            $controler_frontend->displayAddComment();
+                        }
+                    /*}
+                    else {
+                        throw new Exception('Aucun identifiant de billet envoyé');
+                    }*/
                 }
             }
             else {
-                listPosts();
+                //listPosts();
+                $controler_frontend->listPosts();
             }
         }
         catch(Exception $e) {
