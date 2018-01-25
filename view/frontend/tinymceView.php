1e8980916c25594d16a66ccc99f905424284f2ba
AkeraKyuar <elise-layes@laposte.net>
2018-01-25 18:09:57 +0100

    french language, save button and link to the posts list added

--- view/frontend/tinymceView.php
+++ view/frontend/tinymceView.php
@@ -2,14 +2,21 @@
 <html>
 <head>
   <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-  <script>tinymce.init({ selector:'textarea' });</script>
+  <script>tinymce.init({ 
+    selector:'textarea',
+    language_url : 'public/js/tinymce/langs/fr_FR.js',
+    language: 'fr_FR',
+    plugins: "save",
+    toolbar: "save"
+   });</script>
 </head>
 <body>
   <form action="index.php?action=sendText" method="post">
     <textarea name="content" id="content">Ecrivez votre texte ici</textarea>
 
-    <input type="submit" value="Publier le billet" />
+    <button type="submitbtn" value="Publier le billet" />
   </form>
+  <p><a href="index.php">Retour au blog</a></p>
 
 </body>
 </html>
