95f83ed011ad12be0204ff2cb4ad9232e43d2944
AkeraKyuar <elise-layes@laposte.net>
2018-02-03 14:53:00 +0100

    file put in the backend folder

---
+++ view/backend/tinymceView.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+      <script>tinymce.init({ 
+        selector:'textarea',
+        language_url : 'public/js/tinymce/langs/fr_FR.js',
+        language: 'fr_FR',
+        plugins: "save",
+        toolbar: "save"
+      });</script>
+    </head>
+    <body>
+      <form action="index.php?action=sendText" method="post">
+        <textarea name="content" id="content">Ecrivez votre texte ici</textarea>
+
+        <button type="submitbtn" value="Publier le billet" />
+      </form>
+      <p><a href="index.php">Retour au blog</a></p>
+      
+  </body>
+</html>
