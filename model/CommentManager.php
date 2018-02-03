0fec6a8e3daa6410d9724f92e7ad36147384834d
AkeraKyuar <elise-layes@laposte.net>
2018-02-03 14:06:24 +0100

    reportComment(), getRepComments(), deleteRepComFromPage() and deleteCom() added

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -21,4 +21,46 @@ class CommentManager extends Manager
         $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
         $comments->execute(array($postId, $author, $comment));
     }
+
+
+    public function reportComment($id)
+    {
+        $db = $this->dbConnect();
+        $repoComments = $db->prepare('UPDATE comments SET reported = 1  WHERE id = ?');
+        $repoComments->execute(array($id));
+    }
+
+
+    public function getRepComments()
+    {
+        $db = $this->dbConnect();
+        $repoComments = $db->query('SELECT id, author, comment FROM comments WHERE reported = 1');
+        
+        return $repoComments;
+    }
+
+    public function getRepComment($id)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id FROM comments WHERE id = ?');
+        $req->execute(array($id));
+        $repoComment = $req->fetch();
+        return $repoComment;
+    }
+
+
+    public function deleteRepComFromPage($id)
+    {
+        $db = $this->dbConnect();
+        $repoComment = $db->prepare('UPDATE comments SET reported = 0 WHERE id = ?');
+        $repoComment->execute(array($id));
+
+    }
+
+    public function deleteCom($id)
+    {
+        $db = $this->dbConnect();
+        $repoComment = $db->prepare('DELETE FROM comments WHERE id = ?');
+        $repoComment->execute(array($id));
+    }
 }
