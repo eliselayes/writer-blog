31ca49c944f928ac411237f84b6b4ae6ea4a62e1
AkeraKyuar <elise-layes@laposte.net>
2018-02-21 10:44:59 +0100

    formatted and header added

--- view/backend/tinymceView.php
+++ view/backend/tinymceView.php
@@ -1,6 +1,28 @@
 <!DOCTYPE html>
   <html>
     <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+      
+    <link href="https://fonts.googleapis.com/css?family=ABeeZee" rel="stylesheet">
+
+    <title>Editeur</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="public/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="public/css/style.css" rel="stylesheet">
+
+
       <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
       <script>tinymce.init({ 
         selector:'textarea',
@@ -10,13 +32,16 @@
         toolbar: "save"
       });</script>
     </head>
-    <body>
+
+    <header id="header-editor">
+      <h1 id="editor-title">Bienvenue Jean !</h1>
+      <p class="editor-description">Inspiré aujourd'hui ?</p>
+    </header>
+
+    <body id="editor">
       <form action="index.php?action=sendText" method="post">
         <textarea name="content" id="content">Ecrivez votre texte ici</textarea>
-
-        <button type="submitbtn" value="Publier le billet" />
       </form>
-      <p><a href="index.php">Retour au blog</a></p>
-      
+      <p id="editor-back"><a href="index.php">Retour au blog</a></p>     
   </body>
 </html>
