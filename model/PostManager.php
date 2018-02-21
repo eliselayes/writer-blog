072a0989126a96d2c56fdc73e8346fe6db929f9c
AkeraKyuar <elise-layes@laposte.net>
2018-02-21 11:11:20 +0100

    methods for the pager, for the archives, to delete a post and to modify a post; all methods changed for the new Manager organisation

--- model/PostManager.php
+++ model/PostManager.php
@@ -4,46 +4,79 @@ namespace OpenClassrooms\Blog\Model;
 
 require_once("model/Manager.php");
 
-class PostManager extends Manager
-{
-    //private $db = $this->dbConnect();
+class PostManager extends Manager {
+    private $cPage;
+    private $nbPostsPerPage = 5;
 
-    public function getPosts()
-    {
-        $db = $this->dbConnect();
-        $req = $db->query('SELECT id, SUBSTRING(content, 1, 150) AS short_content, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS creation_date_fr FROM posts ORDER BY id DESC LIMIT 0, 5');
+    public function getTotalPages() {
+        $sql = 'SELECT COUNT(*) AS totalPosts FROM posts';
+        $req = $this->executeRequest($sql);
+        while ($data = $req->fetch()) {
+            $totalPosts = $data['totalPosts'];
+        }
+        $totalPages = ceil($totalPosts / $this->nbPostsPerPage);
+        return $totalPages;
+    }
 
+    public function getCPage($page) {
+        $totalPages = $this->getTotalPages();
+        $this->cPage = intval($page);
+        return $this->cPage;
+    }
+
+    public function getPosts() {
+        $limit = ($this->cPage - 1)*$this->nbPostsPerPage;
+        $sql = 'SELECT id, SUBSTRING(content, 1, 250) AS short_content, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS creation_date_fr FROM posts ORDER BY id DESC LIMIT ' . $limit . ',' . $this->nbPostsPerPage;
+        $req = $this->executeRequest($sql);
         return $req;
     }
-    public function getPost($postId)
-    {
-        $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, content, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS creation_date_fr FROM posts WHERE id = ?');
-        $req->execute(array($postId));
-        $post = $req->fetch();
 
+    public function getPost($postId) {
+        $sql = 'SELECT id, content, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS creation_date_fr FROM posts WHERE id = ?';
+        $req = $this->executeRequest($sql, array($postId));
+        $post = $req->fetch();
         return $post;
     }
 
-    public function sendText($content)
-    {
-        $db = $this->dbConnect();
-        $posts = $db->prepare('INSERT INTO posts(content, date_creation) VALUES(?, NOW())');
-        $posts->execute(array($content));
+    public function sendText($content) {
+        $sql = 'INSERT INTO posts(content, date_creation) VALUES(?, NOW())';
+        $req = $this->executeRequest($sql, array($content));
+        $posts = $req->fetch();
     }
 
+    public function getMonths() {
+        $sql = 'SELECT MONTH(date_creation) AS m, 
+        CASE WHEN MONTH(date_creation) = 1 THEN \'janvier\' 
+        WHEN MONTH(date_creation) = 2 THEN \'février\' 
+        WHEN MONTH(date_creation) = 3 THEN \'mars\' 
+        WHEN MONTH(date_creation) = 4 THEN \'avril\'
+        WHEN MONTH(date_creation) = 5 THEN \'mai\'
+        WHEN MONTH(date_creation) = 6 THEN \'juin\'
+        WHEN MONTH(date_creation) = 7 THEN \'juillet\'
+        WHEN MONTH(date_creation) = 8 THEN \'août\'
+        WHEN MONTH(date_creation) = 9 THEN \'septembre\'
+        WHEN MONTH(date_creation) = 10 THEN \'octobre\'
+        WHEN MONTH(date_creation) = 11 THEN \'novembre\'
+        WHEN MONTH(date_creation) = 12 THEN \'décembre\'  
+        END AS mois, 
+        YEAR(date_creation) AS y FROM posts GROUP BY y, m';
+        $months = $this->executeRequest($sql);
+        return $months;
+    }
 
+    public function getPostsPerMonth($m) {
+        $sql = 'SELECT id, SUBSTRING(content, 1, 150) AS short_content, DATE_FORMAT(date_creation, \'%d/%m/%Y\') AS creation_date_fr FROM posts WHERE MONTH(date_creation) = ?';
+        $postsPerMonth = $this->executeRequest($sql, array($m));
+        return $postsPerMonth;
+    }
 
-    /*public function nbPagesCount()
-    {
-        $postsPerPage = 5;
-        $db = $this->dbConnect();
-        $req = $db->query('SELECT COUNT(*) AS total FROM posts');
-        $data = $req->fetch(PDO::FETCH_ASSOC);
-        $totalPosts = $data['total'];
-        $totalPages = ceil($totalPosts/$postsPerPage);
+    public function deletePost($id) {
+        $sql = 'DELETE FROM posts WHERE id = ?';
+        $postToBeDel = $this->executeRequest($sql, array($id));
+    }
 
-        return $totalPages;
-    }*/
-
+    public function modifyPost($content, $id) {
+        $sql = 'UPDATE posts SET content = ? WHERE id = ?';
+        $postToBeMod = $this->executeRequest($sql, array($content, $id));
+    }
 }
