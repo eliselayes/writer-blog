98832e3f624cc9dd0ac67bd32e552e2c7d21be26
AkeraKyuar <elise-layes@laposte.net>
2018-02-21 11:19:09 +0100

    methods for the page and for the archives added

--- model/Frontend.php
+++ model/Frontend.php
@@ -2,88 +2,78 @@
 
 namespace OpenClassrooms\Blog\Model;
 
-// Chargement des classes
-
 require_once('model/PostManager.php');
 require_once('model/LogManager.php');
 require_once('model/CommentManager.php');
 
 
-class Frontend extends Manager
-{
+class Frontend extends Manager {
     private $_postmanager;
     private $_logManager;
     private $_commentManager;
 
-    public function __construct()
-    {
+    public function __construct() {
         // initialisation
         $this->_postManager = new \OpenClassrooms\Blog\Model\PostManager();
         $this->_logManager = new \OpenClassrooms\Blog\Model\LogManager();
         $this->_commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
     }
 
-    public function listPosts1()
-    {
-        //$currentPage = 1;
+    public function listPosts1($page) {
+        $totalPages = $this->_postManager->getTotalPages();
+        $cPage = $this->_postManager->getCPage($page);
         $posts = $this->_postManager->getPosts();
-        //$totalpages = $this->_postManager->nbpagesCount();
+        $months = $this->_postManager->getMonths();
         require('view/frontend/listPostsView.php');
-
-        //return $totalpages;
     }
 
-    /*public function listPostsOtherPages()
-    {
-        $currentPage = $_GET['p'];
+    public function changePage($page) {
+        $totalPages = $this->_postManager->getTotalPages();
+        $cPage = $this->_postManager->getCPage($page);
         $posts = $this->_postManager->getPosts();
-        //$totalpages = $this->_postManager->nbpagesCount();
+        $months = $this->_postManager->getMonths();
         require('view/frontend/listPostsView.php');
+    }
 
-        return $totalpages;
-    }*/
-
-
-
-
-    public function seeOnePost($content)
-    {
+    public function seeOnePost($content) {
         $post = $this->_postManager->getPost($content);
         $comments = $this->_commentManager->getComments($content);
-
+        $months = $this->_postManager->getMonths();
         require('view/frontend/postView.php');
     }
 
+    public function seePostsPerMonth($m) {
+        $postsPerMonth = $this->_postManager->getPostsPerMonth($m);
+        $months = $this->_postManager->getMonths();
+        require('view/frontend/postsPerMonthView.php');
+    }
 
 
 
-    public function addComment($postId, $author, $comment)
-    {
+
+    public function addComment($postId, $author, $comment) {
         $this->_commentManager->postComment($postId, $author, $comment);
         echo '<script>alert("Votre commentaire a bien été envoyé");</script>';
         // after the add, display the post with the comments (including the new one)
         $post = $this->_postManager->getPost($postId);
         $comments = $this->_commentManager->getComments($postId);
-
+        $months = $this->_postManager->getMonths();
         require('view/frontend/postView.php');
     }
 
-
-    public function displayAddComment()
-    {
+    public function displayAddComment() {
         require('view/frontend/logView.php');
     }
 
 
 
-    public function addReport($id, $postId)
-    {
+    public function addReport($id, $postId) {
         $this->_commentManager->reportComment($id);
+        
         echo '<script>alert("Le commentaire a bien été signalé");</script>';
-
+        $months = $this->_postManager->getMonths();
         $post = $this->_postManager->getPost($postId);
         $comments = $this->_commentManager->getComments($postId);
-
         require('view/frontend/postView.php');
     }
 
