a25e4b265469146865a58768c27c1aa242fce3ce
AkeraKyuar <elise-layes@laposte.net>
2018-02-21 11:14:50 +0100

    method executeRequest added

--- model/Manager.php
+++ model/Manager.php
@@ -2,12 +2,34 @@
 
 namespace OpenClassrooms\Blog\Model;
 
-class Manager
-{
-    protected function dbConnect()
-    {
-        $db = new \PDO('mysql:host=localhost;dbname=writer_blog;charset=utf8', 'root', 'root');
+abstract class Manager {
+        // PDO object to access db
+        private $db;
+      
+        // exec a sql request eventually parameterized
+        protected function executeRequest($sql, $params = null) {
+          if ($params == null) {
+            $result = $this->getDb()->query($sql);    // direct execution
+          }
+          else {
+            $result = $this->getDb()->prepare($sql);  // prepared request
+            $result->execute($params);
+          }
+          return $result;
+        }
+      
+        // Return a connection object to the db initializing the connexion if needed
+        private function getDb() {
+          if ($this->db == null) {
+            // Creation of the connexion
+            $this->db = new \PDO('mysql:host=localhost;dbname=writer_blog;charset=utf8', 'root', 'root');
+          }
+          return $this->db;
+        }
+      
+    
 
-        return $db;
-    }
+
+
+
 }
