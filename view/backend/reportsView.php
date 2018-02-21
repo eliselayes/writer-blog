c9b2454c23f058f95315b11e37ee94dee39662b2
AkeraKyuar <elise-layes@laposte.net>
2018-02-21 10:47:12 +0100

    formatted

--- view/backend/reportsView.php
+++ view/backend/reportsView.php
@@ -3,31 +3,31 @@
 <?php ob_start(); ?>
 <div class="container">
     <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
 
-        <div class="col-sm-12 blog-header">
-            <h1 class="blog-title">Le blog de Billet simple pour l'Alaska</h1>
-            <p class="lead blog-description">Roman-feuilleton</p>
+            <?php
+                while ($comment = $repoComments->fetch()) {
+            ?>
+                <div class="commentsRep">
+                <p><strong><?= htmlspecialchars($comment['author']) ?></strong></p>
+                <p><?= htmlspecialchars($comment['comment']) ?></p>
+                <div class="comments">
+                    <form action="index.php?action=keepComment&amp;id=<?= $comment['id'] ?>" method="post">
+                        <div> <input type="submit" value="conserver"/></div>    
+                    </form>
+                    <form action="index.php?action=deleteComment&amp;id=<?= $comment['id'] ?>" method="post">
+                        <div class="supp">
+                            <input type="submit" value="supprimer"/>
+                        </div>
+                    </form>
+                </div>
+                </div>
+
+            <?php
+                }
+            ?>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-5 col-sm-offset-4">
-            <p><a href="index.php">Retour à l'accueil du blog</a></p>
-
-            <?php
-            while ($comment = $repoComments->fetch()) {
-            ?>
-                <p><strong><?= htmlspecialchars($comment['author']) ?></p>
-                <p><strong><?= htmlspecialchars($comment['comment']) ?></p>
-                <form action="index.php?action=keepComment&amp;id=<?= $comment['id'] ?>" method="post">
-                    <div> <input type="submit" value="conserver"/></div>    
-                </form>
-                <form action="index.php?action=deleteComment&amp;id=<?= $comment['id'] ?>" method="post">
-                    <div> <input type="submit" value="supprimer"/></div>
-                </form>
-
-            <?php
-            }
-            ?>
+        <div class="col-sm-3">
         </div>
     </div>
 </div>
