2beb597627b05e3c30f20ce30c41cef22357315d
AkeraKyuar <elise-layes@laposte.net>
2018-02-25 15:05:22 +0100

    meta and icon added

--- view/template.php
+++ view/template.php
@@ -5,9 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <meta name="description" content="Episodes du roman Billet simple pour l'Alaska de Jean Forteroche">
+    <link rel="icon" href="public/images/favicon.ico">
       
     <link href="https://fonts.googleapis.com/css?family=ABeeZee" rel="stylesheet">
 
