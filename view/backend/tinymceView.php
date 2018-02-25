2642e242cffdd3dc78c6dbb215dc6498e7eaa959
AkeraKyuar <elise-layes@laposte.net>
2018-02-25 15:13:57 +0100

    title changed, meta and icon added

--- view/backend/tinymceView.php
+++ view/backend/tinymceView.php
@@ -5,13 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+
+    <link rel="icon" href="public/images/favicon.ico">
       
     <link href="https://fonts.googleapis.com/css?family=ABeeZee" rel="stylesheet">
 
-    <title>Editeur</title>
+    <title>Editeur de texte</title>
 
     <!-- Bootstrap core CSS -->
     <link href="public/css/bootstrap.min.css" rel="stylesheet">
